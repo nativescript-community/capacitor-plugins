d71c6cf0234971c3812ad03d96b36cc2093fb080
rdlabo <sakakibara@rdlabo.jp>
2021-02-18 18:48:19 +0900

    chore

--- demo/angular/src/nativescript/references.d.ts
+++ demo/angular/src/nativescript/references.d.ts
@@ -2,12 +2,11 @@
 /// <reference path="./node_modules/@nativescript/types-ios/index.d.ts" />
 /// <reference path="./node_modules/@nativescript/types-android/lib/android-29.d.ts" />
 
-import type { NSPlugin } from '@ns-capacitor/plugins';
 import type { nativeCustom } from '../native-custom';
 
 declare global {
   var androidCapacitorActivity: android.app.Activity;
-  var native: NodeJS.Global & typeof globalThis & NSPlugin & nativeCustom;
+  var native: NodeJS.Global & typeof globalThis & nativeCustom;
 }
 
 
