64dd98fe23fdf34b0190d1f2465041b60f91fe25
rdlabo <sakakibara@rdlabo.jp>
2021-02-18 21:34:31 +0900

    chore

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-export { NSPlugins } from './interfaces';
 export { dreamBig } from './examples/simple';
 export { openNativeModalView } from './examples/modal';
 export { brightness } from './plugins/brightness';
